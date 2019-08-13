0ba5ed31b09f7210b583017cf655ee50282cd9e3
Neologyc <jaroslav@hlavinka.cz>
2019-08-13 22:26:31 +0200

    Update ttfb.sql

--- ttfb.sql
+++ ttfb.sql
@@ -1,8 +1,8 @@
 SELECT
   origin,
-  concat( CAST( round(fast_ttfb*100) AS STRING) , "%") fast_ttfb,
-  concat( CAST( round(avg_ttfb*100) AS STRING) , "%") avg_ttfb,
-  concat( CAST( round(slow_ttfb*100) AS STRING) , "%") slow_ttfb
+  concat( CAST( round(fast_ttfb*100) AS STRING) , "%") fast__ttfb,
+  concat( CAST( round(avg_ttfb*100) AS STRING) , "%") avg__ttfb,
+  concat( CAST( round(slow_ttfb*100) AS STRING) , "%") slow__ttfb
 FROM `chrome-ux-report.materialized.metrics_summary`
 WHERE
 date = '2019-07-01' AND
@@ -13,9 +13,9 @@ origin LIKE "https://www.firmy.cz"
 # SQL pro vás a vaši konkurenci a se seřazením od nejčastější fast_ttfb
 SELECT
   origin,
-  concat( CAST( round(fast_ttfb*100) AS STRING) , "%") fast_ttfb,
-  concat( CAST( round(avg_ttfb*100) AS STRING) , "%") avg_ttfb,
-  concat( CAST( round(slow_ttfb*100) AS STRING) , "%") slow_ttfb
+  concat( CAST( round(fast_ttfb*100) AS STRING) , "%") fast__ttfb,
+  concat( CAST( round(avg_ttfb*100) AS STRING) , "%") avg__ttfb,
+  concat( CAST( round(slow_ttfb*100) AS STRING) , "%") slow__ttfb
 FROM `chrome-ux-report.materialized.metrics_summary`
 WHERE
 date = '2019-07-01' AND 
@@ -26,4 +26,19 @@ date = '2019-07-01' AND
   origin LIKE "https://www.mall.cz" OR 
   origin LIKE "https://www.czc.cz"
 )
-ORDER BY fast_ttfb DESC
+ORDER BY fast_ttfb DESC LIMIT 100
+
+
+# vytáhnout top 100 nejčastěji rychlých domén (na TTFB) (ale není to prováženo návštěvností)
+SELECT
+  origin,
+  concat( CAST( round(fast_ttfb*100) AS STRING) , "%") fast__ttfb,
+  concat( CAST( round(avg_ttfb*100) AS STRING) , "%") avg__ttfb,
+  concat( CAST( round(slow_ttfb*100) AS STRING) , "%") slow__ttfb
+FROM `chrome-ux-report.materialized.metrics_summary`
+WHERE
+date = '2019-07-01' AND 
+(
+  origin LIKE "%.cz" AND origin LIKE "https://%"
+)
+ORDER BY fast_ttfb DESC LIMIT 100
